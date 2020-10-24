dfdbacaf734b42b1d3bc70f4d0a66180132d26e5
Dmitry Olyenyov <dmitry.olyenyov@gmail.com>
2020-10-24 20:27:24 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { createEvent, Store } from "effector";
+
+export type Config = {
+  prefixKey?: string;
+  storage?: Storage;
+  expireMs?: number;
+};
+type PersistedState = [number, any];
+
+export const withPersist = <State>(store: Store<State>, config?: Config) => {
+  const name = store.shortName;
+  const persistKey = config?.prefixKey ? `${config.prefixKey}:${name}` : name;
+  const rehydrate = createEvent("@PERSIST/REHYDRATE");
+
+  const persistedState = config.storage.getItem(persistKey);
+
+  if (persistedState) {
+    const parsedState = parseStoredState(persistedState);
+
+    if (config.expireMs && parsedState[0] + config.expireMs > config.expireMs) {
+      config.storage.removeItem(persistKey);
+    } else {
+      store.on(rehydrate, () => parsedState[1]);
+      rehydrate();
+    }
+  }
+
+  store.watch((state) => {
+    config.storage.setItem(persistKey, JSON.stringify([Date.now(), state]));
+  });
+
+  return store;
+};
+/**
+ * const withPersist = makeWithPersist({prefixKey:"myroot", storage:sessionStorage})
+ *
+ * const myStore$ = withPersist(createStore(...))
+ */
+export const makeWithPersist = (config: Config) => <State>(
+  store: Store<State>
+) => withPersist(store, config);
+
+const parseStoredState = (str: string) => JSON.parse(str) as PersistedState;
