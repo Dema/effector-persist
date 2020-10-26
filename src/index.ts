98ed291da275ae831dc9c66d768831829b75acfb
Dmitry Olyenyov <dmitry.olyenyov@gmail.com>
2020-10-26 15:53:22 +0300

    refactored rehydrate

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ type PersistedState = [number, any];
 export const withPersist = <State>(store: Store<State>, config?: Config) => {
   const name = store.shortName;
   const persistKey = config?.prefixKey ? `${config.prefixKey}:${name}` : name;
-  const rehydrate = createEvent("@PERSIST/REHYDRATE");
+  const rehydrate = createEvent<State>("@PERSIST/REHYDRATE");
+  store.on(rehydrate, (_state, value) => value);
 
   const persistedState = config.storage.getItem(persistKey);
 
@@ -20,8 +21,7 @@ export const withPersist = <State>(store: Store<State>, config?: Config) => {
     if (config.expireMs && parsedState[0] + config.expireMs > config.expireMs) {
       config.storage.removeItem(persistKey);
     } else {
-      store.on(rehydrate, () => parsedState[1]);
-      rehydrate();
+      rehydrate(parsedState[1]);
     }
   }
 
